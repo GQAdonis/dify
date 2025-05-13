b2ed0093ce69c9e5b43766293b40b00658b484aa
Travis James <travis@tribehealthsolutions.com>
2025-05-13 02:44:25 -0500

    added back in uuid fix

--- api/extensions/ext_database.py
+++ api/extensions/ext_database.py
@@ -1,8 +1,12 @@
 from dify_app import DifyApp
 from models import db
+from sqlalchemy import text
 
 
 def init_app(app: DifyApp):
     db.init_app(app)
     with app.app_context():
+        # Enable the uuid-ossp extension for PostgreSQL
+        db.session.execute(text('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'))
+        db.session.commit()
         db.create_all()
